63595ae8776e13f12b48449baaa1033884db0d5c
alexyekymov <alexyekymov@gmail.com>
2023-02-19 19:54:21 +0200

    fix: select userId via post method to UserServlet

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -62,11 +62,6 @@ public class MealServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String action = request.getParameter("action");
-        String userId = request.getParameter("userId");
-
-        if (StringUtils.hasLength(userId)) {
-            SecurityUtil.setAuthUserId(Integer.parseInt(userId));
-        }
 
         switch (action == null ? "all" : action) {
             case "delete":
