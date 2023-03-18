080fb870131f56818aab3d94679c35f532b2326c
alexyekymov <alexyekymov@gmail.com>
2023-03-19 01:15:20 +0200

    feat: HW4 impl meal repo and fix tests

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,19 +1,51 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import org.hibernate.validator.constraints.Range;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+@Entity
+@Table(name = "meal")
+@NamedQueries({
+        @NamedQuery(name = Meal.DELETE,
+                query = "DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:userId"),
+        @NamedQuery(name = Meal.ALL_SORTED, query = "SELECT m FROM Meal m" +
+                " WHERE m.user.id=:userId ORDER BY m.dateTime DESC"),
+        @NamedQuery(name = Meal.BETWEEN_HALF_OPEN,
+                query = "SELECT m FROM Meal m" +
+                        " WHERE m.dateTime>=:startDateTime AND m.dateTime<:endDateTime" +
+                        " AND m.user.id=:userId ORDER BY m.dateTime DESC"),
+
+})
 public class Meal extends AbstractBaseEntity {
+
+    public static final String DELETE = "Meal.delete";
+    public static final String ALL_SORTED = "Meal.allSorted";
+    public static final String BETWEEN_HALF_OPEN = "Meal.betweenHalfOpen";
+
+
+    @Column(name = "date_time")
+    @NotNull
     private LocalDateTime dateTime;
 
+    @Column(name = "description")
+    @NotBlank
+    @Size(min = 3, max = 128)
     private String description;
 
+    @Column(name = "calories")
+    @NotNull
+    @Range(min = 10, max = 10000)
     private int calories;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
 
     public Meal() {
